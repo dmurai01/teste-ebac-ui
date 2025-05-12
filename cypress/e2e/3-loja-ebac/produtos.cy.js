f1e0730f41a7ef85a5ef64f8038e558929f32882
Daniela Murai <dany.murai@gmail.com>
2025-05-12 16:39:07 -0300

    teste produtos lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.name')
+            //.first()
+            //.eq(4)
+            .contains('Gym')
+            .click()
+
+        cy.get('.product_title').should('contain', 'Gym')    
+    });
+
+});
